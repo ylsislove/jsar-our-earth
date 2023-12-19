3471227f1b9152c8412c7ffce2d9d70aa5cf9c62
wangyu <916491013@qq.com>
2023-12-19 22:55:50 +0800

    stash

--- lib/main.ts
+++ lib/main.ts
@@ -1,7 +1,94 @@
+import { getEarthMat, getEarthPlaneMat, getEarthOrbitPlaneMat } from './matManager';
 const scene = spaceDocument.scene as BABYLON.Scene;
-const animationGroups = scene.animationGroups.filter((ag) => ag.name.endsWith('#model'));
+const sun = scene.getNodeById('model') as BABYLON.Mesh;
+const earth = scene.getNodeById('earth') as BABYLON.Mesh;
 
-if (animationGroups.length >= 1) {
-  animationGroups[0].start(true);
+function clg(params:any) {
+  console.log('ylsislove', params);
 }
 
+// // 定义地球距太阳的距离
+// const earth2Sun = 30;
+// let earthAngle = 0;
+// let earthX = earth2Sun * Math.cos(earthAngle);
+// let earthY = earth2Sun * Math.sin(earthAngle);
+
+// // 创建地球
+// const earth = BABYLON.MeshBuilder.CreateSphere('earth', {
+//   diameter: 6,
+// }, scene);
+
+// // 创建地轴
+// const earthAxis = BABYLON.MeshBuilder.CreateCylinder('earthAxis', {
+//   height: 8,
+//   diameter: 0.2,
+//   tessellation: 64
+// }, scene);
+// earthAxis.setParent(earth);
+
+// // 创建地球赤道平面
+// const earthPlane = BABYLON.MeshBuilder.CreateDisc("earthPlane", {
+//   radius: 6,
+//   tessellation: 64,
+//   sideOrientation: BABYLON.Mesh.DOUBLESIDE
+// }, scene);
+// earthPlane.setParent(earth);
+
+// // 创建地球公转轨道平面
+// const earthOrbitPlane = BABYLON.MeshBuilder.CreateDisc("earthOrbitPlane", {
+//   radius: earth2Sun,
+//   tessellation: 64,
+//   sideOrientation: BABYLON.Mesh.DOUBLESIDE
+// }, scene);
+// earth.setParent(earthOrbitPlane);
+
+// 设置材质
+getEarthMat().then(mat => earth.material = mat);
+// getEarthPlaneMat().then(mat => earthPlane.material = mat);
+// getEarthOrbitPlaneMat().then(mat => earthOrbitPlane.material = mat);
+
+// // 设置位置
+// earthOrbitPlane.rotate(new BABYLON.Vector3(1, 0, 0), (90/180*Math.PI), BABYLON.Space.LOCAL);
+// earth.position = new BABYLON.Vector3(earthX, earthY, 0);
+// earth.rotate(new BABYLON.Vector3(1, 0, 0), -(90/180*Math.PI), BABYLON.Space.LOCAL);
+// earth.rotate(new BABYLON.Vector3(0, 0, 1), (23.5/180*Math.PI), BABYLON.Space.LOCAL);
+// earthPlane.rotate(new BABYLON.Vector3(1, 0, 0), (90/180*Math.PI), BABYLON.Space.LOCAL);
+
+// sun.rotate(new BABYLON.Vector3(1, 0, 0), -(90/180*Math.PI), BABYLON.Space.LOCAL);
+
+
+
+// // 动画设置
+// scene.registerBeforeRender(function () {
+//   earthAngle += 1/24/365;
+//   earthX = earth2Sun * Math.cos(earthAngle);
+//   earthY = earth2Sun * Math.sin(earthAngle);
+//   // 地球自转
+//   earth.rotate(new BABYLON.Vector3(0, -1, 0), 1/24, BABYLON.Space.LOCAL);
+//   // 地球公转
+//   earth.position = new BABYLON.Vector3(earthX, earthY, 0);
+//   // earth.rotateAround(sun.position, new BABYLON.Vector3(0, 0, 1), 1/24/10);
+//   // earth.rotate(new BABYLON.Vector3(0, 1, 0), 1/24/10, BABYLON.Space.WORLD);
+//   // 太阳自转
+//   sun.rotate(new BABYLON.Vector3(0, 0, -1), 1/24/25.38, BABYLON.Space.LOCAL);
+// })
+
+// const earthRotationAnimation = new BABYLON.Animation(
+//   'earthRotation',
+//   'rotation',
+//   60,
+//   BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
+//   BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+// );
+// const keyFrames = []; 
+// keyFrames.push({
+//   frame: 0,
+//   value: earth.rotation
+// });
+// keyFrames.push({
+//   frame: 60,
+//   value: earth.rotation.add(new BABYLON.Vector3(0, 2*Math.PI, 0))
+// });
+// earthRotationAnimation.setKeys(keyFrames);
+// earth.animations.push(earthRotationAnimation);
+// scene.beginAnimation(earth, 0, 60, true, 0.25);
---
+++ lib/matManager.ts
@@ -0,0 +1,55 @@
+import earth from '../texture/earth.png';
+
+export async function getEarthMat() {
+    const texture = await matBuilder(earth);
+    // 创建材质
+    const material = new BABYLON.StandardMaterial('matEarth', spaceDocument.scene);
+    material.diffuseTexture = texture;
+    // 返回材质
+    return material;
+};
+
+export async function getEarthPlaneMat() {
+    // 地球平面材质
+    const material = new BABYLON.StandardMaterial('matEarthPlane', spaceDocument.scene);
+    material.diffuseColor = new BABYLON.Color3(0, 0, 1);
+    material.alpha = 0.5;
+    material.transparencyMode = BABYLON.Material.MATERIAL_ALPHABLEND;
+    // 返回材质
+    return material;
+};
+
+export async function getEarthOrbitPlaneMat() {
+    // 地球平面材质
+    const material = new BABYLON.StandardMaterial('matEarthOrbitPlane', spaceDocument.scene);
+    material.diffuseColor = new BABYLON.Color3(1, 1, 0);
+    material.alpha = 0.5;
+    material.transparencyMode = BABYLON.Material.MATERIAL_ALPHABLEND;
+    // 返回材质
+    return material;
+};
+
+export async function matBuilder(image: any) {
+    // 读取图片
+    const bitmap = await createImageBitmap(new Blob([image], { type: 'image/png' }))
+    // 创建画布
+    const canvas = new OffscreenCanvas(bitmap.width, bitmap.height);
+    const ctx = canvas.getContext('2d');
+    // 绘制图片
+    ctx.drawImage(bitmap, 0, 0);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    // 创建纹理
+    const texture = new BABYLON.RawTexture(
+        imageData.data,
+        imageData.width,
+        imageData.height,
+        BABYLON.Engine.TEXTUREFORMAT_RGBA,
+        spaceDocument.scene,
+        false,
+        false,
+        BABYLON.Texture.TRILINEAR_SAMPLINGMODE
+    );
+    // 返回纹理
+    return texture;
+};
+
